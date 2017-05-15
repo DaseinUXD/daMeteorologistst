ee47be9167267b9889a4d4e056c287a053a62403
Mark W. Matthews <mark@digital-alchemy.com>
2017-05-15 17:24:16 -0500

    added bootstrap web forms bundle and forecast.io

--- daMeteorologist/Global.asax
+++ daMeteorologist/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="daMeteorologist.Global" Language="C#" %>
+<%@ Application Codebehind="Global.asax.cs" Inherits="daMeteorologist.Global" Language="C#" %>
--- daMeteorologist/Global.asax.cs
+++ daMeteorologist/Global.asax.cs
@@ -1,20 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
+using System.Web.Optimization;
 
 namespace daMeteorologist
 {
-    public class Global : HttpApplication
-    {
-        void Application_Start(object sender, EventArgs e)
-        {
-            // Code that runs on application startup
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-    }
+	public class Global : HttpApplication
+	{
+		void Application_Start(object sender, EventArgs e)
+		{
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		void Application_End(object sender, EventArgs e)
+		{
+		}
+
+		void Application_Error(object sender, EventArgs e)
+		{
+		}
+	}
 }
